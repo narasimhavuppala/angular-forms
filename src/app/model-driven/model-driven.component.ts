dad990d669a9a59797488a1885ad81a145bcec29
narasimhavuppala <nraovuppala@Wgmail.com>
2018-08-28 10:39:15 +0530

    Data Binding to Typescript Models

--- src/app/model-driven/model-driven.component.ts
+++ src/app/model-driven/model-driven.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-model-driven',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./model-driven.component.css']
 })
 export class ModelDrivenComponent implements OnInit {
+
+  username: FormControl = new FormControl('');
+  password: FormControl = new FormControl('');
 
   constructor() { }
 
--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateDrivenComponent implements OnInit {
 
+  username: String;
+  password: String;
+
   constructor() { }
 
   ngOnInit() {
